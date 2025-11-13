4b074ec318208a7b8f5949018cf402fd4ae83a73
allen-xavier <60575502+allen-xavier@users.noreply.github.com>
2025-11-13 09:56:52 -0300

    Simplify backend startup retries

---
+++ backend/src/notifications/notifications.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { IsString } from 'class-validator';
+import { NotificationsService } from './notifications.service';
+
+class SendCodeDto {
+  @IsString()
+  phone!: string;
+
+  @IsString()
+  code!: string;
+}
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private readonly notifications: NotificationsService) {}
+
+  @Post('whatsapp-code')
+  send(@Body() body: SendCodeDto) {
+    return this.notifications.sendWhatsappCode(body.phone, body.code);
+  }
+}
