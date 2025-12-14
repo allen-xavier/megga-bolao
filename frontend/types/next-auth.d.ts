478908f2dd51bb64c98c5949be524e50bfdd7a1b
allen-xavier <60575502+allen-xavier@users.noreply.github.com>
2025-12-14 10:38:47 -0300

    v0.27

---
+++ frontend/types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import { DefaultSession, DefaultUser } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      accessToken?: string;
+      refreshToken?: string;
+      role?: string;
+    } & DefaultSession['user'];
+  }
+
+  interface User extends DefaultUser {
+    accessToken?: string;
+    refreshToken?: string;
+    role?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken?: string;
+    refreshToken?: string;
+    role?: string;
+  }
+}
